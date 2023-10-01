92e53968c0d42d3c9397b456b7eadf67f861ef43
Rafaabenhassine <rafaabenhassine1@gmail.com>
2023-10-01 22:49:41 +0100

    update NavB

--- src/Components/NavB.js
+++ src/Components/NavB.js
@@ -10,19 +10,22 @@ function NavB({setInputSearch,setInputStars}) {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
-        
+        <Navbar.Brand style={{color:"red"}} href="#">NETFLIX</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
             style={{ maxHeight: '100px' }}
+            navbarScroll
           
           >
             <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action1">Profile</Nav.Link>
+            <Nav.Link href="#action1">Contact</Nav.Link>
+            <Nav.Link href="#action1">Login</Nav.Link>
             <Nav.Link href="#action1">explore</Nav.Link>
             <Nav.Link href="#action2">Link</Nav.Link>
             
+
             <NavDropdown.Divider />
             </Nav>
           <Form className="d-flex">
